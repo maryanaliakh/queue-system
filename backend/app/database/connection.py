c16dd553b64590b65d89059926bcad753b9f0de5
Mariana Liakh <mariana.liakh@Marianas-MacBook-Pro.local>
2026-05-10 17:39:37 +0200

    fix: restore database connection content

---
+++ backend/app/database/connection.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from dotenv import load_dotenv
+import os
+from sqlalchemy.orm import declarative_base
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
